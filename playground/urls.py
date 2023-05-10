e5b76dcc7d7345ac87ec6753c03a967124db1185
aasurjya <ahandique@gmail.com>
2023-05-10 18:55:27 +0000

    frontend-html-done

---
+++ playground/urls.py
@@ -0,0 +1,9 @@
+# playground/urls.py
+
+from django.urls import path
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
---
+++ playground/views.py
@@ -0,0 +1,6 @@
+# playground/views.py
+
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
